823af390b76296b2f0052a4d476f9c985ac2bfa4
Eloy Durán <eloy.de.enige@gmail.com>
2013-01-28 15:04:28 +0100

    Initial import.

---
+++ lib/ios-sim-test.rb
@@ -0,0 +1,9 @@
+require "ios-sim-test/version"
+
+module Ios
+  module Sim
+    module Test
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ios-sim-test/version.rb
@@ -0,0 +1,7 @@
+module Ios
+  module Sim
+    module Test
+      VERSION = "0.0.1"
+    end
+  end
+end
